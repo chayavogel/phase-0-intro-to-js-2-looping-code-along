266158bb73a34e7fba19f753393ee16b9fba3d0f
chaya <chayavogel1@gmail.com>
2023-07-20 13:05:03 -0400

    complete lab in phase1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    let messagesArray = [];
+    for (let i = 0; i < name.length; i++) {
+        messagesArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    console.log(messagesArray);
+    return messagesArray;
+}
+
+function countDown(integer) {
+    let i = integer;
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  return i;
+}
